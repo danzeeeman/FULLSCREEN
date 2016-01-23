ab829ee3ff309dc1bd2b4afe9a6fbde804739cea
Dan Moore <dan@makeitdoathing.com>
2016-01-23 16:42:21 -0500

    tweaks to add onError loop to reload page

--- app/src/main/java/com/xpo/fullscreen/WEB.java
+++ app/src/main/java/com/xpo/fullscreen/WEB.java
@@ -4,6 +4,8 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import android.annotation.SuppressLint;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.KeyEvent;
@@ -19,11 +21,24 @@ import android.webkit.JavascriptInterface;
 import android.webkit.SslErrorHandler;
 import android.net.http.SslError;
 import android.view.GestureDetector;
+import android.webkit.WebResourceError;
+import android.webkit.WebResourceRequest;
+
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.common.api.GoogleApiClient;
+
 import java.util.GregorianCalendar;
-@SuppressLint({ "SetJavaScriptEnabled", "NewApi" })
+
+@SuppressLint({"SetJavaScriptEnabled", "NewApi"})
 public class WEB extends BASE {
 
 	private WebView mWebView;
+	/**
+	 * ATTENTION: This was auto-generated to implement the App Indexing API.
+	 * See https://g.co/AppIndexing/AndroidStudio for more information.
+	 */
+	private GoogleApiClient client;
 
 
 	@Override
@@ -31,35 +46,39 @@ public class WEB extends BASE {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.fullscreen);
 		mWebView = (WebView) findViewById(R.id.webView1);
-        mWebView.setOnLongClickListener(new View.OnLongClickListener() {
-            int count;
-            long lastPress = 0;
-            long firstPress = 0;
-            @Override
-            public boolean onLongClick(View view){
-                Log.i("WEB", "onLongClick");
-                if(lastPress == 0) {
-                    lastPress = System.currentTimeMillis();
-                }
-                if(lastPress - System.currentTimeMillis()<2000 && lastPress != 0) {
-                    String htmlString = getString(R.string.html_file);
-                    if (htmlString.contains("https://") || htmlString.contains("http://")) {
-                        loadRemoteHtmlPage(htmlString);
-                    } else {
-                        loadLocalHtmlPage(getHtmlFromAsset());
-                    }
-                }
-                lastPress = System.currentTimeMillis();
-                return true;
-            }
-        });
+		mWebView.setOnLongClickListener(new View.OnLongClickListener() {
+			int count;
+			long lastPress = 0;
+			long firstPress = 0;
+
+			@Override
+			public boolean onLongClick(View view) {
+				Log.i("WEB", "onLongClick");
+				if (lastPress == 0) {
+					lastPress = System.currentTimeMillis();
+				}
+				if (lastPress - System.currentTimeMillis() < 2000 && lastPress != 0) {
+					String htmlString = getString(R.string.html_file);
+					if (htmlString.contains("https://") || htmlString.contains("http://")) {
+						loadRemoteHtmlPage(htmlString);
+					} else {
+						loadLocalHtmlPage(getHtmlFromAsset());
+					}
+				}
+				lastPress = System.currentTimeMillis();
+				return true;
+			}
+		});
 		// mWebView.loadUrl("http://hotairballoon-on-whitesmoke.com/html5/index.html");
-        String htmlString = getString(R.string.html_file);
-        if(htmlString.contains("https://") || htmlString.contains("http://")){
-            loadRemoteHtmlPage(htmlString);
-        }else {
-            loadLocalHtmlPage(getHtmlFromAsset());
-        }
+		String htmlString = getString(R.string.html_file);
+		if (htmlString.contains("https://") || htmlString.contains("http://")) {
+			loadRemoteHtmlPage(htmlString);
+		} else {
+			loadLocalHtmlPage(getHtmlFromAsset());
+		}
+		// ATTENTION: This was auto-generated to implement the App Indexing API.
+		// See https://g.co/AppIndexing/AndroidStudio for more information.
+		client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
 	}
 
 	@Override
@@ -67,9 +86,9 @@ public class WEB extends BASE {
 		super.onResume();
 		// mWebView.loadUrl("http://hotairballoon-on-whitesmoke.com/html5/index.html");
 		String htmlString = getString(R.string.html_file);
-		if(htmlString.contains("https://") || htmlString.contains("http://")){
+		if (htmlString.contains("https://") || htmlString.contains("http://")) {
 			loadRemoteHtmlPage(htmlString);
-		}else {
+		} else {
 			loadLocalHtmlPage(getHtmlFromAsset());
 		}
 	}
@@ -101,11 +120,11 @@ public class WEB extends BASE {
 
 	@Override
 	public boolean onKeyLongPress(int keyCode, KeyEvent event) {
-        Log.i("WEB", "onKeyLongPress");
+		Log.i("WEB", "onKeyLongPress");
 		String htmlString = getString(R.string.html_file);
-		if(htmlString.contains("https://") || htmlString.contains("http://")){
+		if (htmlString.contains("https://") || htmlString.contains("http://")) {
 			loadRemoteHtmlPage(htmlString);
-		}else {
+		} else {
 			loadLocalHtmlPage(getHtmlFromAsset());
 		}
 
@@ -114,34 +133,42 @@ public class WEB extends BASE {
 	}
 
 	private void loadRemoteHtmlPage(String uri) {
+        final String url = uri;
 		if (uri != null) {
 
-            // Settings
-            WebSettings webSettings = mWebView.getSettings();
-            webSettings.setJavaScriptEnabled(true);
-            webSettings.setAllowFileAccessFromFileURLs(true);
-            webSettings.setAllowUniversalAccessFromFileURLs(true);
+			// Settings
+			WebSettings webSettings = mWebView.getSettings();
+			webSettings.setJavaScriptEnabled(true);
+			webSettings.setAllowFileAccessFromFileURLs(true);
+			webSettings.setAllowUniversalAccessFromFileURLs(true);
 
-            int currentapiVersion = android.os.Build.VERSION.SDK_INT;
-            if (currentapiVersion >= android.os.Build.VERSION_CODES.JELLY_BEAN) {
-                webSettings.setAllowFileAccessFromFileURLs(true);
-                webSettings.setAllowUniversalAccessFromFileURLs(true);
-            }
+			int currentapiVersion = Build.VERSION.SDK_INT;
+			if (currentapiVersion >= Build.VERSION_CODES.JELLY_BEAN) {
+				webSettings.setAllowFileAccessFromFileURLs(true);
+				webSettings.setAllowUniversalAccessFromFileURLs(true);
+			}
 
-            webSettings.setAllowFileAccess(true);
-            webSettings.setAllowContentAccess(true);
+			webSettings.setAllowFileAccess(true);
+			webSettings.setAllowContentAccess(true);
 
 
-            // Add JS interface to allow calls from webview to Android
-            // code. See below for WebAppInterface class implementation
-            mWebView.addJavascriptInterface(new WebAppInterface(this), "Android");
+			// Add JS interface to allow calls from webview to Android
+			// code. See below for WebAppInterface class implementation
+			mWebView.addJavascriptInterface(new WebAppInterface(this), "Android");
 
-            // Set a web view client and a chrome client
-            mWebView.setWebViewClient(new WebViewClient(){
+			// Set a web view client and a chrome client
+			mWebView.setWebViewClient(new WebViewClient() {
 				@Override
-				public void onReceivedSslError (WebView view, SslErrorHandler handler, SslError error) {
+				public void onReceivedSslError(WebView view, SslErrorHandler handler, SslError error) {
 					handler.proceed();
 				}
+
+				@Override
+				public void onReceivedError(WebView view, WebResourceRequest request, WebResourceError error) {
+                    super.onReceivedError(view, request, error);
+                    Log.e(String.valueOf(error.getErrorCode()), error.getDescription().toString());
+                    view.loadUrl(url);
+                }
 			});
 
 			mWebView.setWebChromeClient(new WebChromeClient() {
@@ -160,45 +187,85 @@ public class WEB extends BASE {
 //                            } else {
 //                                request.deny();
 //                            }
-                        }
-                    });
-                }
-            });
-            webSettings.setBuiltInZoomControls(false);
-            mWebView.requestFocusFromTouch();
-            mWebView.loadUrl(uri);
-        }
-		else
+						}
+					});
+				}
+			});
+			webSettings.setBuiltInZoomControls(false);
+			mWebView.requestFocusFromTouch();
+			mWebView.loadUrl(uri);
+		} else
 			Log.i("ERROROROOROROROR", "Cannot Find Web");
 	}
 
 	private void loadLocalHtmlPage(String htmlString) {
-        if (htmlString != null) {
-            WebSettings webSettings = mWebView.getSettings();
-            webSettings.setJavaScriptEnabled(true);
-            webSettings.setBuiltInZoomControls(false);
-            mWebView.requestFocusFromTouch();
-            mWebView.setWebViewClient(new WebViewClient());
-            mWebView.setWebChromeClient(new WebChromeClient());
-            mWebView.loadDataWithBaseURL("file:///android_asset/320/", htmlString, "text/html", "UTF-8", null);
+		if (htmlString != null) {
+			WebSettings webSettings = mWebView.getSettings();
+			webSettings.setJavaScriptEnabled(true);
+			webSettings.setBuiltInZoomControls(false);
+			mWebView.requestFocusFromTouch();
+			mWebView.setWebViewClient(new WebViewClient());
+			mWebView.setWebChromeClient(new WebChromeClient());
+			mWebView.loadDataWithBaseURL("file:///android_asset/320/", htmlString, "text/html", "UTF-8", null);
 
-		}else {
+		} else {
 			Log.i("ERROROROOROROROR", "Cannot Find HTML");
 		}
-    }
-    // Interface b/w JS and Android code
-    private class WebAppInterface {
-        Context mContext;
+	}
 
-        WebAppInterface(Context c) {
-            mContext = c;
-        }
+	@Override
+	public void onStart() {
+		super.onStart();
 
-        // This function can be called in our JS script now
-        @JavascriptInterface
-        public void showToast(String toast) {
-            Toast.makeText(mContext, toast, Toast.LENGTH_LONG).show();
-        }
-    }
+		// ATTENTION: This was auto-generated to implement the App Indexing API.
+		// See https://g.co/AppIndexing/AndroidStudio for more information.
+		client.connect();
+		Action viewAction = Action.newAction(
+				Action.TYPE_VIEW, // TODO: choose an action type.
+				"WEB Page", // TODO: Define a title for the content shown.
+				// TODO: If you have web page content that matches this app activity's content,
+				// make sure this auto-generated web page URL is correct.
+				// Otherwise, set the URL to null.
+				Uri.parse("http://host/path"),
+				// TODO: Make sure this auto-generated app deep link URI is correct.
+				Uri.parse("android-app://com.xpo.fullscreen/http/host/path")
+		);
+		AppIndex.AppIndexApi.start(client, viewAction);
+	}
+
+	@Override
+	public void onStop() {
+		super.onStop();
+
+		// ATTENTION: This was auto-generated to implement the App Indexing API.
+		// See https://g.co/AppIndexing/AndroidStudio for more information.
+		Action viewAction = Action.newAction(
+				Action.TYPE_VIEW, // TODO: choose an action type.
+				"WEB Page", // TODO: Define a title for the content shown.
+				// TODO: If you have web page content that matches this app activity's content,
+				// make sure this auto-generated web page URL is correct.
+				// Otherwise, set the URL to null.
+				Uri.parse("http://host/path"),
+				// TODO: Make sure this auto-generated app deep link URI is correct.
+				Uri.parse("android-app://com.xpo.fullscreen/http/host/path")
+		);
+		AppIndex.AppIndexApi.end(client, viewAction);
+		client.disconnect();
+	}
+
+	// Interface b/w JS and Android code
+	private class WebAppInterface {
+		Context mContext;
+
+		WebAppInterface(Context c) {
+			mContext = c;
+		}
+
+		// This function can be called in our JS script now
+		@JavascriptInterface
+		public void showToast(String toast) {
+			Toast.makeText(mContext, toast, Toast.LENGTH_LONG).show();
+		}
+	}
 }
 
