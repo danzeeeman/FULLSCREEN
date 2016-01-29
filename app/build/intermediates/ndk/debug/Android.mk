b52e3ac4135936b43f72040279f1e0c55b22ee5f
Dan Moore <dan@makeitdoathing.com>
2016-01-29 18:17:55 -0500

    updates to tunnel

--- app/build/intermediates/ndk/debug/Android.mk
+++ app/build/intermediates/ndk/debug/Android.mk
@@ -4,15 +4,15 @@ include $(CLEAR_VARS)
 LOCAL_MODULE := gif
 LOCAL_LDFLAGS := -Wl,--build-id
 LOCAL_SRC_FILES := \
-	/Users/dantheman/src/android/FULLSCREEN-AS/app/src/main/jni/Android.mk \
-	/Users/dantheman/src/android/FULLSCREEN-AS/app/src/main/jni/Application.mk \
-	/Users/dantheman/src/android/FULLSCREEN-AS/app/src/main/jni/gif.c \
-	/Users/dantheman/src/android/FULLSCREEN-AS/app/src/main/jni/giflib/dgif_lib.c \
-	/Users/dantheman/src/android/FULLSCREEN-AS/app/src/main/jni/giflib/gif_err.c \
-	/Users/dantheman/src/android/FULLSCREEN-AS/app/src/main/jni/giflib/gif_hash.c \
-	/Users/dantheman/src/android/FULLSCREEN-AS/app/src/main/jni/giflib/gifalloc.c \
+	/Users/dantheman/src/android/final-apps/tablet/app/src/main/jni/Android.mk \
+	/Users/dantheman/src/android/final-apps/tablet/app/src/main/jni/Application.mk \
+	/Users/dantheman/src/android/final-apps/tablet/app/src/main/jni/gif.c \
+	/Users/dantheman/src/android/final-apps/tablet/app/src/main/jni/giflib/dgif_lib.c \
+	/Users/dantheman/src/android/final-apps/tablet/app/src/main/jni/giflib/gif_err.c \
+	/Users/dantheman/src/android/final-apps/tablet/app/src/main/jni/giflib/gif_hash.c \
+	/Users/dantheman/src/android/final-apps/tablet/app/src/main/jni/giflib/gifalloc.c \
 
-LOCAL_C_INCLUDES += /Users/dantheman/src/android/FULLSCREEN-AS/app/src/main/jni
-LOCAL_C_INCLUDES += /Users/dantheman/src/android/FULLSCREEN-AS/app/src/debug/jni
+LOCAL_C_INCLUDES += /Users/dantheman/src/android/final-apps/tablet/app/src/main/jni
+LOCAL_C_INCLUDES += /Users/dantheman/src/android/final-apps/tablet/app/src/debug/jni
 
 include $(BUILD_SHARED_LIBRARY)
